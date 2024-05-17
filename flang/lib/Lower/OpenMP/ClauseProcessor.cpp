33e726ed0d41dd2abb7b99eb413ea9bc014184d1
Sergio Afonso <safonsof@amd.com>
2024-05-17 11:38:36 +0100

    [Flang][OpenMP] Update flang with changes to the OpenMP dialect
    This patch applies fixes after the updates to OpenMP clause operands, as well
    as updating some tests that were impacted by changes to the ordering or
    assembly format of some clauses in MLIR.

--- flang/lib/Lower/OpenMP/ClauseProcessor.cpp
+++ flang/lib/Lower/OpenMP/ClauseProcessor.cpp
@@ -990,7 +990,7 @@ bool ClauseProcessor::processEnter(
 }
 
 bool ClauseProcessor::processUseDeviceAddr(
-    mlir::omp::UseDeviceClauseOps &result,
+    mlir::omp::UseDeviceAddrClauseOps &result,
     llvm::SmallVectorImpl<mlir::Type> &useDeviceTypes,
     llvm::SmallVectorImpl<mlir::Location> &useDeviceLocs,
     llvm::SmallVectorImpl<const semantics::Symbol *> &useDeviceSyms) const {
@@ -1002,7 +1002,7 @@ bool ClauseProcessor::processUseDeviceAddr(
 }
 
 bool ClauseProcessor::processUseDevicePtr(
-    mlir::omp::UseDeviceClauseOps &result,
+    mlir::omp::UseDevicePtrClauseOps &result,
     llvm::SmallVectorImpl<mlir::Type> &useDeviceTypes,
     llvm::SmallVectorImpl<mlir::Location> &useDeviceLocs,
     llvm::SmallVectorImpl<const semantics::Symbol *> &useDeviceSyms) const {
